ef03e5c1cfdd41d14e7e82bc618ee7604d1d4f72
Kevin <keyeh@calpoly.edu>
2017-04-25 17:25:43 -0700

    initial

---
+++ env.h
@@ -0,0 +1,16 @@
+// Hardware controller ID for I2C
+#define HWC_I2C_ID 45
+
+// Pin assignments
+#define GROW_LIGHT 13
+#define PELTIER 9
+#define PELTIER_FAN 6
+#define WATER_PUMP 5
+#define AIR_SENSOR 2
+
+struct AirSensorData {
+  float temperature;
+  float humidity;
+  unsigned long last_read;
+  bool last_read_success;
+};
